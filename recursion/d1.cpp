fe88e1120bc019375fb7950975b5aabd4dc0a1ce
Ayush Sinha <ayush40451k@gmail.com>
2026-04-20 14:27:19 +0000

    add

--- recursion/d1.cpp
+++ recursion/d1.cpp
@@ -8,8 +8,24 @@ void printN(int n){
     cout << n << " ";
     printN(n - 1);
 }
+// printing 0 to n in increasing order using recursion
+void printNIncreasing(int n){
+    if(n < 0) return;
+    printNIncreasing(n - 1);
+    cout << n << " ";
+}
+
+// factorrial of n using recurion 
+
+int fact(int n){
+    if(n == 0) return 1;
+    return n*fact(n - 1);
+}
 
 int main(){
     printN(5);
+    cout << endl;
+    printNIncreasing(5);
+    cout << endl;
     return 0;
 }
