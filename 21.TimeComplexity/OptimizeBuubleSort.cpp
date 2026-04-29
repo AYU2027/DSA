791a9fa1c8fe7bab125125fa639724e10d3f547a
Ayush Sinha <ayush40451k@gmail.com>
2026-04-29 06:33:27 +0000

    add

---
+++ 21.TimeComplexity/OptimizeBuubleSort.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void OptimizeBubbleSort(vector<int> &arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++) {
+        bool swapped = false;
+        
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true; 
+            }
+        }
+
+       
+        if (!swapped) {
+            break;
+        }
+    }
+}
+
+int main() {
+    vector<int> arr = {5, 4, 3, 2, 1};
+
+    
+    OptimizeBubbleSort(arr);
+
+    // Print the sorted original vector
+    for (int x : arr) {
+        cout << x << ' ';
+    }
+    cout << endl;
+
+    return 0;
+}
+
